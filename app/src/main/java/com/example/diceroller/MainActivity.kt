b91cf566b424285959f8b420e6c455b5b8a3f414
Richard Salvaty <richardsalvaty@hotmail.com>
2019-09-16 12:13:15 -0700

    added better graphical dice, removed old dice xmls, updated image src

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -24,12 +24,12 @@ class MainActivity : AppCompatActivity() {
         val randomInt = Random().nextInt(6) + 1
 
         val drawableResource = when(randomInt){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            else -> R.drawable.dice6
         }
 
         diceImage.setImageResource(drawableResource)
